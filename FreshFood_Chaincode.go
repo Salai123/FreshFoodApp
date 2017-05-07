0f9081436333dff548cc68b170641169402ca2eb
Salai123 <salmadha@in.ibm.com>
2017-05-07 19:47:32 -0400

    init

--- FreshFood_Chaincode.go
+++ FreshFood_Chaincode.go
@@ -136,9 +136,6 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	if function == "read" { //read a variable
 		return t.read(stub, args)
 	}
-                  if function == "sread" { //read a variable
-		return t.sread(stub, args)
-	}
 
 	fmt.Println("query did not find func: " + function)
 
@@ -393,27 +390,4 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string,)
 	return resAsBytes,nil
                              
 }
-// read - query function to read key/value pair
-func (t *SimpleChaincode) sread(stub shim.ChaincodeStubInterface, args []string,) ([]byte,error) {
-	var key,jsonResp string
-	var err error                    
-	if len(args) != 1 {
-		return nil,errors.New("Incorrect number of arguments. Expecting name of the key to query")
-	}
 
-                   
-          
-	key = args[0]
-                                     
-                
-	valuex,err := stub.GetState(key)
-                   if err != nil {
-		jsonResp = "{\"Error\":\"Failed to get state for " + key+ "\"}"
-		return nil, errors.New(jsonResp)
-	}
-
-                   
-                          
-	return valuex,nil
-                             
-}
